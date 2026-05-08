2bfa06776d781d2074dd60730caae2e7ca235ad5
Yeetnade <shreyassundar0305@gmail.com>
2026-05-07 20:42:50 -0700

    Started learning react native and implementing frontend for the home page.

--- Frontend/convoysync/app/(tabs)/_layout.tsx
+++ Frontend/convoysync/app/(tabs)/_layout.tsx
@@ -1,35 +1,41 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
+import {Ionicons} from "@expo/vector-icons"
+import { Text, View } from 'react-native'
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+const tabsLayout = () => {
+    return (
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: "red",
+            tabBarInactiveTintColor: "green",
+            tabBarStyle: {
+                backgroundColor: "black",
+                borderTopWidth: 1,
+                borderTopColor: "yellow",
+                height: 90,
+                paddingBottom: 30,
+                paddingTop: 10,
+            },
+        }}>
+            <Tabs.Screen 
+                name='home' 
+                options= {{
+                    title: "My Journeys", 
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name='home' size={size} color={color}/>
+                    ),
+                }} 
+            />
+            <Tabs.Screen 
+                name='archivedTrips' 
+                options= {{
+                    title: "Saved Trips", 
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name='archive' size={size} color={color}/>
+                    )
+                }} 
+            />
+        </Tabs>
+    )
 }
+
+export default tabsLayout;
---
+++ Frontend/convoysync/app/(tabs)/archivedTrips.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const archivedTrips = () => {
+    return (
+        <View>
+            <Text>Past Trips</Text>
+        </View>
+    )
+}
+
+export default archivedTrips;
