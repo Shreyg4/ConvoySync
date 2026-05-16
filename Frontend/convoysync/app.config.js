f97b87536512e00a4a78e0661e5aacf4bd2f01a5
Yeetnade <shreyassundar0305@gmail.com>
2026-05-15 18:50:04 -0700

    getting a dev build working on andriod and ios sims

--- Frontend/convoysync/app.config.js
+++ Frontend/convoysync/app.config.js
@@ -1,18 +1,31 @@
 import 'dotenv/config';
 
-export default ({ config }) => ({
+export default ({ config }) => {
+  const iosMapsApiKey = process.env.IOS_MAPS_API_KEY || process.env.MAPS_API_KEY;
+  const androidMapsApiKey =
+    process.env.ANDROID_MAPS_API_KEY || process.env.MAPS_API_KEY;
+
+  return {
     ...config,
     ios: {
-        ...(config.ios || {}),
-        config: { ...config.ios?.config, googleMapsApiKey: process.env.MAPS_API_KEY },
+      ...(config.ios || {}),
+      bundleIdentifier:
+        process.env.IOS_BUNDLE_IDENTIFIER || config.ios?.bundleIdentifier,
+      config: {
+        ...config.ios?.config,
+        googleMapsApiKey: iosMapsApiKey,
+      },
     },
     android: {
-        ...(config.android || {}),
-        config: {
-            ...config.android?.config,
-            googleMaps: {
-                apiKey: process.env.MAPS_API_KEY,
-            },
+      ...(config.android || {}),
+      package: process.env.ANDROID_PACKAGE || config.android?.package,
+      config: {
+        ...config.android?.config,
+        googleMaps: {
+          ...config.android?.config?.googleMaps,
+          apiKey: androidMapsApiKey,
         },
+      },
     },
-});
+  };
+};
