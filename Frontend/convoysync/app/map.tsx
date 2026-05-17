4af203903f2618287860e2db7c82090fef011f98
Yeetnade <shreyassundar0305@gmail.com>
2026-05-16 19:39:46 -0700

    added location tracking

--- Frontend/convoysync/app/map.tsx
+++ Frontend/convoysync/app/map.tsx
@@ -1,10 +1,35 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
 import BackHeader from '@/components/BackHeader';
 import { globalStyles } from '../styles';
+import { useEffect, useRef } from 'react';
 
 const Map = () => {
+    const mapRef = useRef<MapView>(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await
+                Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.log('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+
+            mapRef.current?.animateToRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.09,
+                longitudeDelta: 0.04,
+            }, 1000);
+        })();
+    }, []);
+
+    useEffect
     return (
         <View style={{ flex: 1 }}>
             <SafeAreaView style={globalStyles.mapContainer}>
@@ -20,6 +45,9 @@ const Map = () => {
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
+                showsUserLocation={true}
+                showsMyLocationButton={true}
+                followsUserLocation={true}
             />
         </View>
     )
